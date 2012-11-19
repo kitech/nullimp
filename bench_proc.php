4f2d52935a7963a44c817b7e09a85889025751d9
liuguangzhao <liuguangzhao@users.sf.net>
2012-11-19 22:35:15 +0000

    abc

---
+++ bench_proc.php
@@ -0,0 +1,32 @@
+<?php
+
+$load_file = "/proc/loadavg";
+
+$times = 10000;
+$btime = microtime(true);
+for ($i = 0; $i < $times; $i++) {
+    // $loads = file_get_contents($load_file);
+    $fp = fopen($load_file, 'r');
+    $loads = fgets($fp);
+    fclose($fp);
+    // $pos1 = strchr($loads, ' ');
+    $ploads = explode(' ', $loads);
+    // $min1_load = $ploads[0];
+    // $min5_load = $ploads[1];
+    // $min15_load = $ploads[2];
+    
+    // $min1_load = strpbrk($loads, ' ');
+}
+
+$etime = microtime(true);
+$dtime = $etime - $btime;
+$rate = $times / $dtime;
+
+echo "used: {$dtime}, rate: {$rate}/sec\n";
+
+/*
+  如果出现这个错误，PHP Warning:  fopen(): open_basedir restriction in effect.
+  检查php.ini的配置，open_basedir = /srv/http/:/home/:/tmp/:/usr/share/pear/
+
+  检测这个值，平均大概9w/秒
+ */
