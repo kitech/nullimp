09b29cc699b76ef833b59417fbc3ff1e1a1450ba
liuguangzhao <liuguangzhao@users.sf.net>
2012-11-27 15:36:59 +0800

    can async proc

--- ngx_nullimp/ngx_nullimp.cpp
+++ ngx_nullimp/ngx_nullimp.cpp
@@ -6,6 +6,8 @@ extern "C" {
 #include <ngx_log.h>
 };
 
+#include <pthread.h>
+
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
@@ -65,7 +67,61 @@ ngx_module_t  ngx_http_nullimp_module = {
     NGX_MODULE_V1_PADDING
 };
 
+static void *imp_thread_proc(void *arg)
+{
+    ngx_int_t rc;
+    ngx_buf_t *b, pb;
+    ngx_chain_t out;
 
+    ngx_http_request_t *r = (ngx_http_request_t *)arg;
+    std::cout<<"222r-main-connt:"<<r->main->count<<std::endl;
+
+    GmImp *imp = new GmImp();
+    imp->process("ab/cb/g/efkjdsoifds_p12_mk456.jpg");
+
+    size_t img_length;
+    u_char *img_data = imp->get_result(&img_length);
+
+    char *ct = "image/jpeg";
+    r->headers_out.content_type.len = strlen(ct);
+    r->headers_out.content_type.data = (u_char*)(ct);
+
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = img_length ; // 5;
+
+    // b = (ngx_buf_t*)ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    b = &pb;
+    out.buf = b;
+    out.next = NULL;
+
+    ngx_log_debug0(NGX_LOG_INFO, r->connection->log, 0, "enter nullimp handler");    
+
+    // 
+    char *resp = "12345";
+    // b->pos = (u_char*)resp;
+    // b->last = (u_char*)(resp + 5);
+
+    b->pos = (u_char*)img_data;
+    b->last = (u_char*)(img_data + img_length);
+
+    b->memory = 1;
+    b->last_buf = 1;
+
+    ngx_log_debug0(NGX_LOG_INFO, r->connection->log, 0, "enter nullimp handler");    
+    rc = ngx_http_send_header(r);
+    ngx_log_debug0(NGX_LOG_INFO, r->connection->log, 0, "enter nullimp handler");    
+
+    rc = ngx_http_output_filter(r, &out);
+    ngx_log_debug0(NGX_LOG_INFO, r->connection->log, 0, "enter nullimp handler");    
+
+    delete imp;
+
+    // sleep(3);
+    std::cout<<"imp thread done111"<<std::endl;
+    ngx_http_finalize_request(r, NGX_DONE);
+    std::cout<<"imp thread done222 "<<std::endl;
+}
 
 //// 实现
 
@@ -84,14 +140,24 @@ static ngx_int_t ngx_http_nullimp_handler(ngx_http_request_t *r)
     ngx_log_debug0(NGX_LOG_INFO, r->connection->log, 0, "enter nullimp handler");    
 
     ////////////
-    GmImp *imp = new GmImp();
+    size_t img_length;
+    u_char *img_data;
+    GmImp *imp;// = new GmImp();
+    /*
     imp->process("ab/cb/g/efkjdsoifds_p12_mk456.jpg");
 
-    size_t img_length;
-    u_char *img_data = imp->get_result(&img_length);
+    img_data = imp->get_result(&img_length);
 
+    std::cout<<"r-main-connt:"<<r->main->count<<std::endl;
     std::cout<<"got img length:"<<img_length<<std::endl;
+    */
+    r->main->count += 1; // 这句代码这么管用啊！！！
 
+    pthread_t pth;
+    int pcr;
+    pcr = pthread_create(&pth, NULL, imp_thread_proc, (void*)r);
+
+    return NGX_OK;
     // delete imp;
     ////////////
 
